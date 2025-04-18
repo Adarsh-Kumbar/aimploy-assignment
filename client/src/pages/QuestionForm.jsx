e87a46fc79fd24760bae26091ecc29ae5e5a037a
Adarsh Kumbar <119795624+Adarsh-Kumbar@users.noreply.github.com>
2025-04-18 12:22:25 +0530

    Update QuestionForm.jsx

--- client/src/pages/QuestionForm.jsx
+++ client/src/pages/QuestionForm.jsx
@@ -35,7 +35,7 @@ const QuestionForm = () => {
     }
 
     axios
-      .post(`https://aimploy-assignment-backend.onrender.com/api/question/${candidateId}`, formData, {
+      .post(`http://localhost:5000/api/question/${candidateId}`, formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
